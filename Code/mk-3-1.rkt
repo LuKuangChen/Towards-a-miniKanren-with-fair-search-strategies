3758d061f44a2858634d36a7fb5796ab4ee28c41
Lu, Kuang-Chen <kl13@iu.edu>
2019-03-07 21:25:34 -0500

    another style of append-map-inf in mk-3

---
+++ Code/mk-3-1.rkt
@@ -0,0 +1,273 @@
+#lang racket
+(provide (all-defined-out))
+#| mk-3 |#
+#|
+
+* The type of immature part becomes a list of thunk
+* Perform BFS search explicitly with queue
+
+|#
+
+; 1/5 CHANGES
+(define (empty-inf) '(() . ()))
+(define (unit-mature-inf v) `((,v) . ()))
+(define (unit-immature-inf th) `(() . (,th)))
+
+(define (null-inf? s-inf)
+  (and (null? (car s-inf))
+       (null? (cdr s-inf))))
+
+(define (mature-inf? s-inf)
+  (pair? (car s-inf)))
+
+(define (car-inf s-inf)
+  (car (car s-inf)))
+
+(define (cdr-inf s-inf)
+  (cons (cdr (car s-inf))
+    (cdr s-inf)))
+
+; 2/5 CHANGES
+; Invoke every thunk
+(define (force-inf s-inf)
+  (let loop ((ths (cdr s-inf)))
+    (cond
+      ((null? ths) (empty-inf))
+      (else (let ((th (car ths)))
+              (append-inf (th)
+                (loop (cdr ths))))))))
+
+#| code below is RI wrt ∞, except append-inf and take |#
+
+(define var (lambda (x) (vector x)))
+(define var? (lambda (x) (vector? x)))
+
+(define empty-s '())
+
+(define (walk v s)
+  (let ((a (and (var? v) (assv v s))))
+    (cond
+      ((pair? a) (walk (cdr a) s))
+      (else v))))
+
+(define (ext-s x v s)
+  (cond
+    ((occurs? x v s) #f)
+    (else (cons `(,x . ,v) s))))
+
+(define (occurs? x v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v) (eqv? v x))
+      ((pair? v) 
+       (or (occurs? x (car v) s)
+           (occurs? x (cdr v) s)))
+      (else #f))))
+
+(define (unify u v s)
+  (let ((u (walk u s)) (v (walk v s)))
+    (cond
+      ((eqv? u v) s)
+      ((var? u) (ext-s u v s))
+      ((var? v) (ext-s v u s))
+      ((and (pair? u) (pair? v))
+       (let ((s (unify (car u) (car v) s)))
+         (and s
+           (unify (cdr u) (cdr v) s))))
+      (else #f))))
+
+(define (== u v)
+  (lambda (s)
+    (let ((s (unify u v s)))
+      (if s (unit-mature-inf s) (empty-inf)))))
+
+(define succeed
+  (lambda (s)
+    (unit-mature-inf s)))
+ 
+(define fail
+  (lambda (s)
+    (empty-inf)))
+
+(define (disj2 g1 g2)
+  (lambda (s)
+    (append-inf (g1 s) (g2 s))))
+
+; 3/5 CHANGES
+(define (append-inf s-inf t-inf)
+  (cons (append (car s-inf) (car t-inf))
+    (append (cdr s-inf) (cdr t-inf))))
+
+; 4/5 CHANGES
+(define (take-inf n s-inf)
+  (take-inf^ n (car s-inf) (cdr s-inf) '()))
+
+; 5/5 CHANGES
+; P and Q together represent a functional queue.
+; The fourth line reshapes the queue.
+; The last line invokes the first thunk of the queue and
+; binds the result to s-inf. In the recursive call, the
+; new vs comes from s-inf. We also dequeue the first
+; thunk (th) and enqueue thunks from s-inf.
+(define (take-inf^ n vs P Q)
+  (cond
+    ((and n (zero? n)) '())
+    ((pair? vs)
+     (cons (car vs)
+       (take-inf^ (and n (sub1 n)) (cdr vs) P Q)))
+    ((and (null? P) (null? Q)) '())
+    ((null? P) (take-inf^ n vs (reverse Q) '()))
+    (else (let ([th (car P)])
+            (let ([s-inf (th)])
+              (take-inf^ n (car s-inf)
+                (cdr P)
+                (append (reverse (cdr s-inf)) Q)))))))
+
+(define (conj2 g1 g2)
+  (lambda (s)
+    (append-map-inf g2 (g1 s))))
+
+(define (append-map-inf g s-inf)
+  (cond
+    ((null-inf? s-inf) (empty-inf))
+    ((mature-inf? s-inf)
+     (append-inf (g (car-inf s-inf))
+       (append-map-inf g (cdr-inf s-inf))))
+    (else
+     (cons '()
+       (map (lambda (th)
+              (lambda ()
+                (append-map-inf g (th))))
+            (cdr s-inf))))))
+
+(define (call/fresh name f)
+  (f (var name)))
+
+(define (reify-name n)
+  (string->symbol
+    (string-append "_"
+      (number->string n))))
+
+(define (walk* v s)
+  (let ((v (walk v s)))
+    (cond
+      ((var? v) v)
+      ((pair? v)
+       (cons
+         (walk* (car v) s)
+         (walk* (cdr v) s)))
+      (else v))))
+#|
+; 'project' is defined in the frame 10:98 on page 166.
+(define-syntax project
+  (syntax-rules ()
+    ((project (x ...) g ...)
+     (lambda (s)
+       (let ((x (walk* x s)) ...)
+         ((conj g ...) s))))))
+|#
+(define (reify-s v r)
+  (let ((v (walk v r)))
+    (cond
+      ((var? v)
+       (let ((n (length r)))
+         (let ((rn (reify-name n)))
+           (cons `(,v . ,rn) r))))
+      ((pair? v)
+       (let ((r (reify-s (car v) r)))
+         (reify-s (cdr v) r)))
+      (else r))))
+
+(define (reify v)
+  (lambda (s)
+    (let ((v (walk* v s)))
+      (let ((r (reify-s v empty-s)))
+        (walk* v r)))))
+
+(define (run-goal n g)
+  (take-inf n (g empty-s)))
+
+(define (ifte g1 g2 g3)
+  (lambda (s)
+    (let loop ((s-inf (g1 s)))
+      (cond
+        ((null-inf? s-inf) (g3 s))
+        ((mature-inf? s-inf)
+         (append-map-inf g2 s-inf))
+        (else (unit-immature-inf
+                (lambda ()
+                  (loop (force-inf s-inf)))))))))
+
+(define (once g)
+  (lambda (s)
+    (let loop ((s-inf (g s)))
+      (cond
+        ((null-inf? s-inf) (empty-inf))
+        ((mature-inf? s-inf)
+         (unit-mature-inf (car-inf s-inf)))
+        (else (unit-immature-inf
+                (lambda ()
+                  (loop (force-inf s-inf)))))))))
+
+
+
+;;; Here are the key parts of Appendix A
+
+(define-syntax disj
+  (syntax-rules ()
+    ((disj) fail)
+    ((disj g) g)
+    ((disj g0 g ...) (disj2 g0 (disj g ...)))))
+
+(define-syntax conj
+  (syntax-rules ()
+    ((conj) succeed)
+    ((conj g) g)
+    ((conj g0 g ...) (conj2 g0 (conj g ...)))))
+
+(define-syntax defrel
+  (syntax-rules ()
+    ((defrel (name x ...) g ...)
+     (define (name x ...)
+       (lambda (s)
+         (unit-immature-inf
+           (lambda ()
+             ((conj g ...) s))))))))
+
+(define-syntax run
+  (syntax-rules ()
+    ((run n (x0 x ...) g ...)
+     (run n q (fresh (x0 x ...)
+                (== `(,x0 ,x ...) q) g ...)))
+    ((run n q g ...)
+     (let ((q (var 'q)))
+       (map (reify q)
+         (run-goal n (conj g ...)))))))
+
+(define-syntax run*
+  (syntax-rules ()
+    ((run* q g ...) (run #f q g ...))))
+
+(define-syntax fresh
+  (syntax-rules ()
+    ((fresh () g ...) (conj g ...))
+    ((fresh (x0 x ...) g ...)
+     (call/fresh 'x_0
+       (lambda (x0)
+         (fresh (x ...) g ...))))))
+
+(define-syntax conde
+  (syntax-rules ()
+    ((conde (g ...) ...)
+     (disj (conj g ...) ...))))
+
+(define-syntax conda
+  (syntax-rules ()
+    ((conda (g0 g ...)) (conj g0 g ...))
+    ((conda (g0 g ...) ln ...)
+     (ifte g0 (conj g ...) (conda ln ...)))))
+
+(define-syntax condu
+  (syntax-rules ()
+    ((condu (g0 g ...) ...)
+     (conda ((once g0) g ...) ...))))
