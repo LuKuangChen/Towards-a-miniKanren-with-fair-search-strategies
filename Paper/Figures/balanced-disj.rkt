3fa9ead2b207ef4f71ddc33b9d6c430962384c78
Lu, Kuang-Chen <kl13@iu.edu>
2019-05-19 00:56:19 +0800

    fix typo in DFSbi

--- Paper/Figures/balanced-disj.rkt
+++ Paper/Figures/balanced-disj.rkt
@@ -1,22 +1,16 @@
-#| [Goal] x ([Goal] x [Goal] -> Goal) -> Goal |#
-(define (split ls k)
-  (cond
-    [(null? ls) (k '() '())]
-    [else (split (cdr ls)
-            (lambda (l1 l2)
-              (k (cons (car ls) l2) l1)))]))
-
-#| [Goal] -> Goal |#
-(define (disj* gs)
-  (cond
-    [(null? (cdr gs)) (car gs)]
-    [else
-     (split gs
-       (lambda (gs1 gs2)
-         (disj2 (disj* gs1)
-                (disj* gs2))))]))
-
 (define-syntax disj
   (syntax-rules ()
     [(disj) fail]
-    [(disj g ...) (disj* (list g ...))]))
+    [(disj g ...) (disj+ (g ...) () ())]))
+
+(define-syntax disj+
+  (syntax-rules ()
+    [(disj+ (g) () ()) g]
+    [(disj+ () (gl ...) (gr ...))
+     (disj2 (disj+ (gl ...) () ())
+            (disj+ (gr ...) () ()))]
+    [(disj+ (g0) (gl ...) (gr ...))
+     (disj2 (disj+ (gl ... g0) () ())
+            (disj+ (gr ...) () ()))]
+    [(disj+ (g0 g1 g ...) (gl ...) (gr ...))
+     (disj+ (g ...) (gl ... g0) (gr ... g1))]))
